39025084ab862a74a2f3ac0134c498ecc3f1c315
AlfredoBejarano <alfredocbejarano@gmail.com>
2020-09-04 16:11:42 -0500

    Create BookListFragment.kt

---
+++ app/src/main/java/me/alfredobejarano/bitsocodechallenge/view/BookListFragment.kt
@@ -0,0 +1,27 @@
+package me.alfredobejarano.bitsocodechallenge.view
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import dagger.hilt.android.AndroidEntryPoint
+import me.alfredobejarano.bitsocodechallenge.viewmodel.BookListViewModel
+
+@AndroidEntryPoint
+class BookListFragment : Fragment() {
+    private val viewModel: BookListViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, parent: ViewGroup?, state: Bundle?) =
+        View(parent?.context)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getBooks().observe(viewLifecycleOwner, Observer {
+            Log.d("Book", it.toString())
+        })
+    }
+}
