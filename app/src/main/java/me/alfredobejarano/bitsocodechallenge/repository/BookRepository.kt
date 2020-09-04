8fa9936cf582d387ce00db497c8cc29b1ac1ecae
AlfredoBejarano <alfredocbejarano@gmail.com>
2020-09-04 15:40:05 -0500

    Create BookRepository.kt

---
+++ app/src/main/java/me/alfredobejarano/bitsocodechallenge/repository/BookRepository.kt
@@ -0,0 +1,62 @@
+package me.alfredobejarano.bitsocodechallenge.repository
+
+import me.alfredobejarano.bitsocodechallenge.datasource.local.BookDao
+import me.alfredobejarano.bitsocodechallenge.datasource.local.CacheManager
+import me.alfredobejarano.bitsocodechallenge.datasource.remote.BitsoApiService
+import me.alfredobejarano.bitsocodechallenge.model.TickerToBookMapper
+import me.alfredobejarano.bitsocodechallenge.model.local.Book
+import javax.inject.Inject
+
+class BookRepository @Inject constructor(
+    private val localDataSource: BookDao,
+    private val cacheManager: CacheManager,
+    private val mapper: TickerToBookMapper,
+    private val remoteDataSource: BitsoApiService
+) {
+    /**
+     * Retrieves the Ticker data of a Book.
+     * @param book Book name to retrieve its data.
+     */
+    private suspend fun getBookTicker(book: String) =
+        remoteDataSource.getBookTicker(book).payload
+
+    /**
+     * Retrieves the available books from Bitso api.
+     * @return List of books from Bitso API.
+     */
+    private suspend fun getRemoteBooks() = remoteDataSource.getAvailableBooks().payload
+        ?.map { getBookTicker(it.book.orEmpty()) }
+        ?.requireNoNulls()?.map(mapper::map)
+        ?: emptyList()
+
+    /**
+     * Receives a list of books and stores them within the local database.
+     * @param books List of books.
+     * @return List of books that were cached.
+     */
+    private suspend fun cacheRemoteBooks(books: List<Book>) = books.apply {
+        forEach { localDataSource.createOrUpdate(it) }
+        cacheManager.createBookCache()
+    }
+
+    /**
+     * Retrieves the list of books.
+     * @return [List] of available books.
+     */
+    suspend fun getBooks() = if (cacheManager.isBookCacheValid()) {
+        localDataSource.readAll()
+    } else {
+        cacheRemoteBooks(getRemoteBooks())
+    } ?: emptyList()
+
+    /**
+     * Retrieves a book.
+     * @param book Book name to retrieve.
+     * @return [Book] object or null if said book can't be found.
+     */
+    suspend fun getBook(book: String) = if (cacheManager.isBookCacheValid()) {
+        localDataSource.read(book)
+    } else {
+        getBookTicker(book)?.run(mapper::map)?.also { localDataSource.createOrUpdate(it) }
+    }
+}
