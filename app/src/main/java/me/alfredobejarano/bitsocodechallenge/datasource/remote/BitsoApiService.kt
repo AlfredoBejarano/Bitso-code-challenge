3010527cadfca4b891b621d651532c82d395fed1
AlfredoBejarano <alfredocbejarano@gmail.com>
2020-09-04 14:27:09 -0500

    Create BitsoApiService.kt

---
+++ app/src/main/java/me/alfredobejarano/bitsocodechallenge/datasource/remote/BitsoApiService.kt
@@ -0,0 +1,23 @@
+package me.alfredobejarano.bitsocodechallenge.datasource.remote
+
+import me.alfredobejarano.bitsocodechallenge.model.remote.AvailableBook
+import me.alfredobejarano.bitsocodechallenge.model.remote.BitsoResponse
+import me.alfredobejarano.bitsocodechallenge.model.remote.Ticker
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BitsoApiService {
+    /**
+     * Fetches a list of available books within Bitso.
+     * @return [BitsoResponse] object containing a [List] of [AvailableBook] objects as its payload.
+     */
+    @GET("/available_books/")
+    suspend fun getAvailableBooks(): BitsoResponse<List<AvailableBook>>
+
+    /**
+     * Retrieves the ticker details of a book.
+     * @param book Book name to retrieve it's ticker (ex. btc_mxn).
+     */
+    @GET("/ticker/")
+    suspend fun getBookTicker(@Query("book") book: String): BitsoResponse<Ticker>
+}
